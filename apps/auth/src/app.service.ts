4490d2b9dcb6e2715044b3f83513a579dd826131
Saikat Dutta <saikatdutta1991@gmail.com>
2021-06-19 16:12:58 +0530

    test

--- apps/auth/src/app.service.ts
+++ apps/auth/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to auth service!';
+    return 'Welcome to auth service';
   }
 }
