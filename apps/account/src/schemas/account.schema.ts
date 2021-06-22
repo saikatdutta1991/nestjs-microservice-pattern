88bcba7d855e6515fbc440d1a93950a0e43ca19d
Saikat Dutta <saikatdutta1991@gmail.com>
2021-06-22 21:29:52 +0530

    Account type added

--- apps/account/src/schemas/account.schema.ts
+++ apps/account/src/schemas/account.schema.ts
@@ -1,10 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { AccountType } from 'shared/enums/account-type.enum';
 
 export type AccountDocument = Account & Document;
 
 @Schema()
 export class Account {
+  @Prop({ type: AccountType, enum: AccountType, default: AccountType.USER })
+  type: AccountType;
+
   @Prop({ required: true })
   username: string;
 
