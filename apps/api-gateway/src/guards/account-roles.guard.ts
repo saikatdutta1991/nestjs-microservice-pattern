59fc8a241ae7e65e46ad464803ec741714efeb5e
Saikat Dutta <saikatdutta1991@gmail.com>
2021-06-23 21:20:17 +0530

    Code cleanup

--- apps/api-gateway/src/guards/account-roles.guard.ts
+++ apps/api-gateway/src/guards/account-roles.guard.ts
@@ -1,12 +1,5 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  Inject,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ClientProxy } from '@nestjs/microservices';
-import { ServiceName } from 'config/service.configuration';
 import { AccountRoles } from 'shared/service-contracts/account/account-roles.';
 import { VerifyAccessTokenOutput } from 'shared/service-contracts/account/commands/verify-access-token/verify-access-token.output';
 import { ROLES_KEY } from '../decorators/roles.decorator';
@@ -14,10 +7,7 @@ import { AccessTokenGuard } from './access-token.guard';
 
 @Injectable()
 export class AccountRolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    @Inject(ServiceName.ACCOUNT) private readonly accountService: ClientProxy,
-  ) {}
+  constructor(private reflector: Reflector) {}
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const accessTokenPayload: VerifyAccessTokenOutput =
