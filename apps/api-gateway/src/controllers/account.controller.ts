834ec319f1e615a1c76794a3153e17f7d4379991
Saikat Dutta <saikatdutta1991@gmail.com>
2021-06-24 20:33:25 +0530

    Get acces token using refresh token

--- apps/api-gateway/src/controllers/account.controller.ts
+++ apps/api-gateway/src/controllers/account.controller.ts
@@ -2,7 +2,6 @@ import {
   BadRequestException,
   Controller,
   Inject,
-  Headers,
   Get,
   Param,
 } from '@nestjs/common';
@@ -11,8 +10,6 @@ import { ServiceName } from 'config/service.configuration';
 import { Account } from 'shared/service-contracts/account/account';
 import { AccountRoles } from 'shared/service-contracts/account/account-roles.';
 import { AccountCommands } from 'shared/service-contracts/account/commands/account.commands';
-import { GetAccessTokenInput } from 'shared/service-contracts/account/commands/get-access-token/get-access-token.input';
-import { GetAccessTokenOutput } from 'shared/service-contracts/account/commands/get-access-token/get-access-token.output';
 import { GetAccountInput } from 'shared/service-contracts/account/commands/get-account/get-account.input';
 import { Auth } from '../decorators/auth.decorator';
 import { SerializeOptions } from '../decorators/serialize.decorator';
@@ -23,19 +20,6 @@ export class AccountController {
   constructor(
     @Inject(ServiceName.ACCOUNT) private readonly accountService: ClientProxy,
   ) {}
-
-  @Get('/access-token')
-  public async getAccessToken(@Headers('refresh-token') refreshToken: string) {
-    return await this.accountService
-      .send<GetAccessTokenOutput, GetAccessTokenInput>(
-        { cmd: AccountCommands.ACCESS_TOKEN },
-        { refreshToken },
-      )
-      .toPromise()
-      .catch((error) => {
-        throw new BadRequestException(error);
-      });
-  }
 
   @Get('/:accountId')
   @Auth(AccountRoles.USER)
--- apps/api-gateway/src/graphql/account/account.queries.ts
+++ apps/api-gateway/src/graphql/account/account.queries.ts
@@ -1,8 +1,18 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { BadRequestException, Inject } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { ClientProxy } from '@nestjs/microservices';
+import { ServiceName } from 'config/service.configuration';
+import { AccountCommands } from 'shared/service-contracts/account/commands/account.commands';
+import { GetAccessTokenInput } from 'shared/service-contracts/account/commands/get-access-token/get-access-token.input';
+import { GetAccessTokenOutput } from 'shared/service-contracts/account/commands/get-access-token/get-access-token.output';
 import { Account } from './models/account.model';
 
 @Resolver(() => Account)
 export class AccountQueries {
+  constructor(
+    @Inject(ServiceName.ACCOUNT) private readonly accountService: ClientProxy,
+  ) {}
+
   @Query(() => Account)
   public async getAccount(): Promise<Account> {
     return {
@@ -11,5 +21,22 @@ export class AccountQueries {
       name: 'Saikat',
       username: 'saikatdutta1991',
     };
+  }
+
+  @Query(() => String)
+  public async getAccessToken(
+    @Args('refreshToken') refreshToken: string,
+  ): Promise<string> {
+    const { accessToken } = await this.accountService
+      .send<GetAccessTokenOutput, GetAccessTokenInput>(
+        { cmd: AccountCommands.ACCESS_TOKEN },
+        { refreshToken },
+      )
+      .toPromise()
+      .catch((error) => {
+        throw new BadRequestException(error);
+      });
+
+    return accessToken;
   }
 }
